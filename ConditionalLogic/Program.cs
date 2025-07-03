2186059ffed5f7f028ac6cd20dbcb137fc10582b
devyanshXmalhotra <devyansh.malhotra@artesian.io>
2025-07-03 17:25:24 +0530

    Add basic if-else conditional logic example

---
+++ ConditionalLogic/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a = 5;
+int b = 5;
+
+if ((a + b) > 10)
+{
+    Console.WriteLine("The sum of a and b is greater than 10.");
+}
+else if ((a + b) < 10)
+{
+    Console.WriteLine("The sum of a and b is less than 10.");
+}
+else
+{
+    Console.WriteLine("The sum of a and b is equal to 10.");
+}
+
+if (a > b)
+{
+    Console.WriteLine("a is greater than b.");
+}
+else
+{
+    Console.WriteLine("a is not greater than b.");
+}
+
