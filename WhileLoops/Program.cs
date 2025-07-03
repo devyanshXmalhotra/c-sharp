389120bb2f53f20a5647148472f9ba738b768702
devyanshXmalhotra <devyansh.malhotra@artesian.io>
2025-07-03 17:48:24 +0530

    Add examples of while and do-while loops

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int count = 9;
+
+while(count>0){
+    Console.WriteLine($"Count is:{count}");
+    count--;
+}
+
+
+//do-while loop
+
+do
+{
+    Console.WriteLine($"Count is:{count}");
+    count++;
+}while(count<10);
+
+
+
+
