a828dbf0c32a7d245abcd3e0cf7d3653b5eb384e
devyanshXmalhotra <devyansh.malhotra@artesian.io>
2025-07-03 18:47:24 +0530

    Add examples of List<string> operations and iteration using for and foreach loops

---
+++ DataCollectionUsingList/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//var=local variable type inference
+
+
+var names= new List<string> {"anuj", "sachin", "priyanka", "sanjay", "suman"};
+
+foreach(var name in names)
+{
+    Console.WriteLine(name);
+}
+
+names.Add("sumanth");
+names.Add("sumanth2");
+
+for (int i=0;i<names.Count;i++)
+{
+    Console.WriteLine(names[i]);
+}
+
+names.Remove("sumanth2");
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
---
+++ ForLoops/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int number = 10;
+
+for (int i = 0; i < number; i++)
+{
+    Console.WriteLine($"Number is:{i+1}");
+}
