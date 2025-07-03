a6e77e1da340bcfec8596af20fc109d6438e8ba3
devyanshXmalhotra <devyansh.malhotra@artesian.io>
2025-07-03 15:39:14 +0530

    Add string operations: Contains, Replace, and ToLower examples

---
+++ StringOperations/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string sentence = "The quick brown fox jumps over the lazy dog";
+Console.WriteLine(sentence.Contains("jump"));
+Console.WriteLine(sentence.Replace("brown fox","tiger"));
+Console.WriteLine(sentence);
+Console.WriteLine(sentence.ToLower());
+Console.WriteLine(sentence);
