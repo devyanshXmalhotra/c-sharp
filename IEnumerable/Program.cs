80a33b1d2f3d1fa9e59bc3a5bc25f59ad134e700
devyanshXmalhotra <devyansh.malhotra@artesian.io>
2025-07-04 12:06:02 +0530

    Add IEnumerable and LINQ query examples for sorting and filtering a list of scores

---
+++ IEnumerable/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+List<int> scores = [ 90, 80, 70, 60, 50, 40, 30, 20, 10 ];
+
+Console.WriteLine($"Score count:{scores.Count}");
+
+IEnumerable<int> score = from n in scores orderby n select n;
+
+foreach(int n in score)
+{
+    Console.WriteLine(n);
+}
+
+IEnumerable<int> scoreQuery = from n in scores where n < 40 select n;
+
+foreach (int n in scoreQuery)
+{
+    Console.WriteLine(n);
+}
